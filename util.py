3228ff9332fcf16b14612b65a47cd72f6f900501
Hugoagogo <Hughpowell2004@yahoo.com.au>
2011-02-28 22:29:52 +1100

    Added a function to generate the points to make up a circle

--- util.py
+++ util.py
@@ -1,2 +1,14 @@
+import math
 def point_dist(p1,p2):
     return ((p1[0]**2 + p2[0]**2)+(p1[1]**2 + p2[1]**2))*0.5
+    
+def generate_circle(radius,pos=(0,0),segments=10):
+    positions = []
+    for seg in range(segments):
+        angle = seg*2*math.pi/segments
+        x = math.sin(angle)*radius
+        y = math.cos(angle)*radius
+        positions.append((x+pos[0],y+pos[1]))
+    return positions
+        
+    
